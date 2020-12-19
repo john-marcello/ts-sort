09aa218b759250b1b369d61599470850587b2c10
coyotewind <coyoteswind@gmail.com>
2020-12-19 12:16:52 -0500

    first commit

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('word up my bitches, rock on');
+var logSomething = function () {
+    console.log('something');
+};
+logSomething();
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log('word up my bitches, rock on');
+
+const logSomething = function () {
+    console.log('something');
+}
+
+logSomething();
